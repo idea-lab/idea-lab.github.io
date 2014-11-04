ab036850055f27f9160c878dca02ea7d80555ea1
eMYKion <eMYKion@users.noreply.github.com>
2014-11-04 16:35:49 -0500

    Got project-list, and project-tiles with some css, & data-binding

---
+++ script.js
@@ -0,0 +1,17 @@
+//makes all the projectobjs in content
+
+//
+//for(var i=0;i<5;i+=1){
+//  document.getElementById("hlw").innerHTML+="<div class=\"projectobj\"><p>TEST</p></div>";
+//}
+document.getElementById("projectList").projectData=[
+  {
+    name:"chromeo",
+    img:"chrome.ico"
+  },
+  {
+    name:"facebooki",
+    img:"facebook.ico"
+  }
+  
+];
